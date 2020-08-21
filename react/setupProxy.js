d9f27cfca841b233eedc48cb505e9b4fb5477a1f
yeshuai0329 <yeshuai0329@aliyun.com>
2020-08-21 17:27:12 +0800

    更新readme说明文件

--- react/setupProxy.js
+++ react/setupProxy.js
@@ -1,23 +1,22 @@
-const httpMiddleware = require('http-proxy-middleware')
+const httpMiddleware = require("http-proxy-middleware");
 
-console.log('kerwin-111', httpMiddleware)
 // node ,vue
 module.exports = function (app) {
   app.use(
-    '/ajax',
+    "/ajax",
     httpMiddleware({
-      target: 'https://m.maoyan.com',
-      changeOrigin: true
+      target: "https://m.maoyan.com",
+      changeOrigin: true,
     })
-  )
+  );
 
   app.use(
-    '/api',
+    "/api",
     httpMiddleware({
-      target: 'https://localhost:5000',
-      changeOrigin: true
+      target: "https://localhost:5000",
+      changeOrigin: true,
     })
-  )
-}
+  );
+};
 
 // /api
